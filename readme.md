d8d2bc6d6351c1186a4e43380177ef4c5a01d549
spaceman02-ops <jkrichardson08@gmail.com>
2021-07-05 19:05:25 -0500

    Initial commit

---
+++ readme.md
@@ -0,0 +1,17 @@
+**Inline Table Rolls**
+
+This module for Foundry Virtual Tabletop allows users to roll on tables within a chat message. Once installed, a user can type the name of the table surrounded by brackets with a leading dash like so:
+
+![Example Image 1](https://i.imgur.com/pDUiZyI.png)
+
+Inline Table Rolls parses messages with the above formatting and outputs the following:
+
+![Example Image 2](https://i.imgur.com/hg206r1.png)
+
+Inline Table Rolls is especially convenient for creating complex encounter and loot instances like so:
+
+![Example Image 3](https://i.imgur.com/5zp2jgq.png)
+
+Inline Table Rolls even rolls recursively through tables within tables and has a built-in recursion limit to prevent infinite loops.
+
+This is my first module, so I apologize for any bugs and I am open to any constructive criticism that will help me improve my project!
---
+++ scripts/inline-tables.js
@@ -0,0 +1,37 @@
+Hooks.on("createChatMessage", (a) => {
+  if (a.data.content.match(/\[\[\#(.*?)\]\]/g)) parseInlineTables(a);
+});
+
+const parseInlineTables = async (a) => {
+  let content = { ...a.data }.content;
+  let finalId = a.data._id;
+  let depth = 0;
+  let newContent = await handleMatches(content, depth);
+  let theMessage = game.messages.get(finalId);
+  await theMessage.update({ content: newContent });
+};
+
+const handleMatches = async (content, depth) => {
+  if (depth > 10) {
+    throw new Error(
+      `Help I'm trapped in a loop! You're calling the same table in a lower table.`
+    );
+  }
+  let matches = content.match(/\[\[\#(.*?)\]\]/g);
+  const lf = new Intl.ListFormat("en");
+  if (matches != null) {
+    for (let match of matches) {
+      let table = match.replace(`[[#`, ``).replace(`]]`, ``);
+      let roll = await game.tables.getName(table).roll();
+      let resArray = [];
+      for (let result of roll.results) {
+        resArray.push(result.getChatText());
+      }
+      content = content.replace(match, lf.format(resArray));
+    }
+    depth += 1;
+    return handleMatches(content, depth);
+  } else if (matches == null) {
+    return content;
+  }
+};
