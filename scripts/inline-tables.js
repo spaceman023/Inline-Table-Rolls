df1a5e1040922559ac62598c7733eb68f94ad7a3
John Richardson <jkrichardson08@gmail.com>
2022-09-23 20:48:44 -0500

    commit

--- scripts/inline-tables.js
+++ scripts/inline-tables.js
@@ -1,77 +1,95 @@
-Hooks.on('createChatMessage', (a) => {
-  if (a.content.match(/\[\[#(.*?)\]\]/g)) parseInlineTables(a);
+Hooks.once('init', async () => {
+  await setDefaultSettings();
 });
 
-async function parseInlineTables(a) {
-  
-  let { content } = { ...a };
-  const finalId = a._id;
-  const depth = 0;
-  let newContent = await handleMatches(content, depth);
-  const theMessage = game.messages.get(finalId);
-        
-  if (theMessage.user._id === game.userId) {
-    await theMessage.update({ content: newContent });
-    newContent = theMessage
-    await theMessage.delete();
-    await ChatMessage.create({
-      ...newContent
-    });
-  }
+Hooks.on('createChatMessage', (messageData) => {
+  if (messageData.content.match(/\[\[#(.*?)\]\]/g)) parseInlineTables(messageData);
+});
+
+async function parseInlineTables(messageData) {
+  const { content } = messageData
+  const tableChatMessage = game.messages.get(messageData._id);
+  if (tableChatMessage.user._id !== game.userId) return;
+  const newContent = await handleMatches(content);
+  const enrichedContent = await TextEditor.enrichHTML(newContent);
+  await tableChatMessage.update({ content: enrichedContent });
 }
 
-async function handleMatches(content, depth) {
+async function handleMatches(content, depth = 0) {
   if (depth > 10) {
+    ui.notifications?.warn(`Help I'm trapped in a loop! You're calling the same table in a lower table.`);
     throw new Error(`Help I'm trapped in a loop! You're calling the same table in a lower table.`);
   }
   const matches = content.match(/\[\[#(.*?)\]\]/g);
   if (matches != null) {
     for (const match of matches) {
-      content = await rollTableAndReplaceContent(content, match);
+      content = await getResults(content, match);
     }
     depth += 1;
     return handleMatches(content, depth);
   }
-        
   return content;
 }
 
-async function rollTableAndReplaceContent(content, match) {
-  let table = match.replace(`[[#`, ``).replace(`]]`, ``);
-  let roll;
-  const resArray = [];
-  const lf = new Intl.ListFormat('en');
-  if (!game.tables.getName(table)) {
-    table = await findCompendiumTable(table);
-  } else {
-
-    table = game.tables.getName(table);
-          
-  }
-  if (table) {
-    roll = await table.draw({ displayChat: false });
-  } else {
-    throw new Error('Table not found');
-  }
+async function getResults(content, match) {
+  const tableName = match.replace(`[[#`, ``).replace(`]]`, ``);
+  const table = await findTable(tableName);
+  const roll = await table.draw({ displayChat: false });
+  let resultArray = [];
   for (const result of roll.results) {
-    resArray.push(result.getChatText());
+    resultArray.push(result.getChatText());
   }
-  return content.replace(match, lf.format(resArray));
+  return content.replace(match, await formatResult(resultArray));
 }
 
-
-async function findCompendiumTable(name) {
-  const pack = game.packs.find((p) => {
-    if (p.index.getName(name)) {
-      return true;
-    }
-    return false;
-  });
-  if (!pack) {
-    return null;
+async function formatResult(result) {
+  const formatMultipleResults = game.settings.get('Inline-Table-Rolls', 'format-multiple-results');
+  if (formatMultipleResults) {
+    const lf = new Intl.ListFormat('en');
+    return lf.format(result);
   }
-  const entry = pack.index.getName(name);
-  const table = await pack.getDocument(entry._id);
-  return table;
+  return result.join(" ");
+}
 
+async function findTable(name) {
+  let table = null;
+  if (game.tables.getName(name)) {
+    table = game.tables.getName(name)
+  } else {
+    const pack = game.packs.find((p) => {
+      if (p.metadata.type !== 'RollTable') return false;
+      return !!p.index.getName(name)
+    });
+    if (pack) {
+      let entry = pack.index.getName(name);
+      table = await pack.getDocument(entry._id);
+    }
+  }
+  if (!table) {
+    ui.notifications?.warn(`Table ${name} not found.`);
+    throw new Error(`Table ${name} not found.`);
+  }
+  return table
+}
+
+async function setDefaultSettings() {
+  const defaultSettings = [
+    {
+      id: 'format-multiple-results',
+      name: 'Format Multiple Results',
+      hint: 'Determines whether the module will format multiple results from a table with commas and conjunctions.',
+      type: Boolean,
+      default: true
+    },
+  ];
+  for (const setting of defaultSettings) {
+    game.settings.register('Inline-Table-Rolls', setting.id, {
+      name: setting.name,
+      hint: setting.hint,
+      scope: 'world',
+      config: true,
+      type: setting.type,
+      default: setting.default,
+    });
+  }
 }
