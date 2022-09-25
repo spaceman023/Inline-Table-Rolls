e26131f7509c1bf40fbf90559c035cd978c1a42c
John Richardson <jkrichardson08@gmail.com>
2022-09-24 20:36:13 -0500

    commit

--- scripts/inline-tables.js
+++ scripts/inline-tables.js
@@ -1,13 +1,16 @@
-Hooks.once('init', async () => {
+Hooks.once("init", async () => {
   await setDefaultSettings();
 });
 
-Hooks.on('createChatMessage', (messageData) => {
+Hooks.on("createChatMessage", messageData => {
   if (messageData.content.match(/\[\[#(.*?)\]\]/g)) parseInlineTables(messageData);
 });
 
+/**
+ * @param {ChatMessage} messageData - The chat message data
+ */
 async function parseInlineTables(messageData) {
-  const { content } = messageData
+  const { content } = messageData;
   const tableChatMessage = game.messages.get(messageData._id);
   if (tableChatMessage.user._id !== game.userId) return;
   const newContent = await handleMatches(content);
@@ -15,6 +18,10 @@ async function parseInlineTables(messageData) {
   await tableChatMessage.update({ content: enrichedContent });
 }
 
+/**
+ * @param {string} content - The content of the identified chat message
+ * @returns {string} - The content with the table results inserted
+ */
 async function handleMatches(content, depth = 0) {
   if (depth > 10) {
     ui.notifications?.warn(`Help I'm trapped in a loop! You're calling the same table in a lower table.`);
@@ -31,34 +38,55 @@ async function handleMatches(content, depth = 0) {
   return content;
 }
 
+/**
+ * This function takes match from the regex, determines how many times to roll the table,
+ * finds the table, rolls it, and replaces the match with the result.
+ *
+ * @param {string} content - The content of the chat message
+ * @param {string} match - The match from the regex
+ * @returns {string} - The content with the match replaced with the results of the table
+ */
 async function getResults(content, match) {
-  const tableName = match.replace(`[[#`, ``).replace(`]]`, ``);
+  const numberToDrawFormula = match.match(/\?(.+)\?/g)?.[0]?.replace(/\?/g, "") || "1";
+  const numberToDraw = await new Roll(numberToDrawFormula).roll();
+  const tableName = match
+    .replace(/\[\[#/g, "")
+    .replace(/\]\]/g, "")
+    .replace(/\?.+\?/g, "");
   const table = await findTable(tableName);
-  const roll = await table.draw({ displayChat: false });
+  const results = await table.drawMany(Number(numberToDraw.total), { displayChat: false });
   let resultArray = [];
-  for (const result of roll.results) {
+  for (const result of results.results) {
     resultArray.push(result.getChatText());
   }
   return content.replace(match, await formatResult(resultArray));
 }
 
+/**
+ * @param {string[]} result - The result of the table roll
+ * @returns {string} - The formatted result
+ */
 async function formatResult(result) {
-  const formatMultipleResults = game.settings.get('Inline-Table-Rolls', 'format-multiple-results');
+  const formatMultipleResults = game.settings.get("Inline-Table-Rolls", "format-multiple-results");
   if (formatMultipleResults) {
-    const lf = new Intl.ListFormat('en');
+    const lf = new Intl.ListFormat("en");
     return lf.format(result);
   }
   return result.join(" ");
 }
 
+/**
+ * @param {string} name - The name of the table
+ * @returns {RollTable} - The RollTable object
+ */
 async function findTable(name) {
   let table = null;
   if (game.tables.getName(name)) {
-    table = game.tables.getName(name)
+    table = game.tables.getName(name);
   } else {
-    const pack = game.packs.find((p) => {
-      if (p.metadata.type !== 'RollTable') return false;
-      return !!p.index.getName(name)
+    const pack = game.packs.find(p => {
+      if (p.metadata.type !== "RollTable") return false;
+      return !!p.index.getName(name);
     });
     if (pack) {
       let entry = pack.index.getName(name);
@@ -69,24 +97,24 @@ async function findTable(name) {
     ui.notifications?.warn(`Table ${name} not found.`);
     throw new Error(`Table ${name} not found.`);
   }
-  return table
+  return table;
 }
 
 async function setDefaultSettings() {
   const defaultSettings = [
     {
-      id: 'format-multiple-results',
-      name: 'Format Multiple Results',
-      hint: 'Determines whether the module will format multiple results from a table with commas and conjunctions.',
+      id: "format-multiple-results",
+      name: "Format Multiple Results",
+      hint: "Determines whether the module will format multiple results from a table with commas and conjunctions.",
       type: Boolean,
-      default: true
+      default: true,
     },
   ];
   for (const setting of defaultSettings) {
-    game.settings.register('Inline-Table-Rolls', setting.id, {
+    game.settings.register("Inline-Table-Rolls", setting.id, {
       name: setting.name,
       hint: setting.hint,
-      scope: 'world',
+      scope: "world",
       config: true,
       type: setting.type,
       default: setting.default,
